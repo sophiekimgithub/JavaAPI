81071143f5048788fda305aa5d7792eb6faa2d04
sophie <sophie.kim.it@gmail.com>
2017-11-30 19:28:58 +1100

    Angular 2

---
+++ WebContent/js/exam7.js
@@ -0,0 +1,23 @@
+var app7 = angular.module('app7', []);
+
+app7.controller('mainCtrl', function() {
+	this.name = "Animal";
+	this.sound = "Grrr";
+	
+	this.animalClick = function() {
+		alert(this.name + " says " + this.sound);
+	};
+	
+});
+
+app7.controller('dogCtrl', function($controller) {
+	var childCtrl = this;
+	
+	childCtrl.child = $controller('mainCtrl', {});
+	
+	childCtrl.child.name = "Dog";
+	childCtrl.child.bark = "Woof";
+	childCtrl.child.dogClick = function() {
+		alert(this.name + " says " + this.sound + " and " + this.bark);
+	}
+});
---
+++ WebContent/js/filters.js
@@ -0,0 +1,6 @@
+angular.module('weatherFilters', []).filter('raining',
+function() {
+	return function(input) {
+		return input? '\u2602' : '\u2600';
+	}
+});
